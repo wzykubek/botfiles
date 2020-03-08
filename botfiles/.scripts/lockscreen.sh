1b68d47ae7f7bf0d3716540c05bc6dade74954cb
samedamci <samedamci@disroot.org>
2020-03-08 19:14:45 +0100

    Edit lockscreen script to working on my both computers

--- botfiles/.scripts/lockscreen.sh
+++ botfiles/.scripts/lockscreen.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/zsh
 
-i3lock -i ~/.config/wallpapers/1920x1080/wall$(( ( RANDOM % $(ls ~/.config/wallpapers/1920x1080 | wc -l) +1 ) )).png --composite --insidecolor=55443311 --ringcolor=99443300 --ringvercolor=00000000 --insidevercolor=55443311 --keyhlcolor=99999999 --indicator --veriftext= --wrongtext=! --radius 180 -b --separatorcolor=00000000 -k --timesize=40 --datesize=20 --time-font=Monospace --date-font=Monospace --bar-indicator
+RESOL=$(xrandr | grep '*' | grep -o '?*[0-9]*x?*[0-9]*')
+
+i3lock -i ~/.config/wallpapers/$RESOL/wall$(( ( RANDOM % $(ls ~/.config/wallpapers/$RESOL | wc -l) +1 ) )).png --composite --insidecolor=55443311 --ringcolor=99443300 --ringvercolor=00000000 --insidevercolor=55443311 --keyhlcolor=99999999 --indicator --veriftext= --wrongtext=! --radius 180 -b --separatorcolor=00000000 -k --timesize=40 --datesize=20 --time-font=Monospace --date-font=Monospace --bar-indicator
